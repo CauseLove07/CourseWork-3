6e4d7f5973524bea048a11674f0dd3184db26a57
Satir <Satir91@e1.ru>
2023-04-27 14:05:41 +0500

    added tests

--- src/main.py
+++ src/main.py
@@ -7,9 +7,9 @@ def main():
     data = load_json(file)
     filtered_data = get_filtered_data(data)
     get_five = get_last_five(filtered_data)
-    compited = get_exchanged_data(get_five)
+    complited = get_exchanged_data(get_five)
 
-    for i in compited:
+    for i in complited:
         print(i)
         print()
 
